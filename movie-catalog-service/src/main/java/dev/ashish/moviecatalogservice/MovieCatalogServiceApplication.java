3b464d4a2d4f44e1653a52745950b1a80021b76c
Ashish Rawat <ashishrawat2911@gmail.com>
2020-10-29 20:56:51 +0530

    added hystrix dashboard

--- movie-catalog-service/src/main/java/dev/ashish/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/dev/ashish/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
